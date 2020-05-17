5a6c8db25d3e35533a2ce55094a5e39287b6f9fe
Holland Pleskac <56204301+HollandPleskac@users.noreply.github.com>
2020-05-17 14:18:31 -0700

    Add files via upload

--- auth_screens/sign_up_screen.dart
+++ auth_screens/sign_up_screen.dart
@@ -74,24 +74,33 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
         String uid = prefs.getString('uid');
 
-        String selectedEventId =
-            await _firestore.collection('user data').document(uid).get().then(
-                  (docSnapShot) => docSnapShot.data['selected event'],
-                );
+// sets the selected event id
+        try {
+          String selectedEventId =
+              await _firestore.collection('user data').document(uid).get().then(
+                    (docSnapShot) => docSnapShot.data['selected event'],
+                  );
+          setSelectedEventId(selectedEventId);
+        } catch (e) {
+          String selectedEventId = 'no selected event id';
+          setSelectedEventId(selectedEventId);
+        }
 
-        // sets the selected event id
-        setSelectedEventId(selectedEventId);
-
-        String selectedEventName = await _firestore
-            .collection('user data')
-            .document(uid)
-            .collection('my events')
-            .document(selectedEventId)
-            .get()
-            .then((docSnap) => docSnap.data['event name']);
-
-        //sets the selected event name
-        setSelectedEventName(uid, selectedEventId, selectedEventName);
+        try {
+          String selectedEventId = prefs.getString('selected event id');
+          String selectedEventName = await _firestore
+              .collection('user data')
+              .document(uid)
+              .collection('my events')
+              .document(selectedEventId)
+              .get()
+              .then((docSnap) => docSnap.data['event name']);
+          setSelectedEventName(uid, selectedEventId, selectedEventName);
+        } catch (e) {
+          String selectedEventId = prefs.getString('selected event id');
+          String selectedEventName = 'no selected event name';
+          setSelectedEventName(uid, selectedEventId, selectedEventName);
+        }
 
         Navigator.push(
           context,
